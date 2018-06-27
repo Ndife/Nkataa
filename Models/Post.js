0297d50da8b85584f5d8bf7f3cdba753b8367ea0
Ndife <stilesndife@gmail.com>
2018-06-27 10:52:28 +0100

    added user and comment to post file

--- Models/Post.js
+++ Models/Post.js
@@ -3,5 +3,8 @@ const Mongoose = require('mongoose');
 let PostSchema = Mongoose.Schema({
     time: Date,
     postBody: String,
-    
+    user: {type:Mongoose.Schema.Types.ObjectId, ref:'User'},
+    comment: {type:Mongoose.Schema.Types.ObjectId, ref:'Comment'}
 });
+
+module.exports = Mongoose.model('Post', PostSchema);
