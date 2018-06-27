cfa1a05ed01f711de2859e2e3cee8cd2cf2c0655
Ndife <stilesndife@gmail.com>
2018-06-27 10:51:37 +0100

    added post link to the User file

--- Models/User.js
+++ Models/User.js
@@ -4,4 +4,7 @@ let UserSchema = Mongoose.Schema({
     name: String,
     email: {type: String, unique:true},
     password: String,
+    posts: [{type:Mongoose.Schema.Types.ObjectId, ref:'Post'}]
 });
+
+module.exports = Mongoose.model('User',UserSchema);
