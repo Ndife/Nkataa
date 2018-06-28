748024ce86f0f3e7d8a943da79bfbf99a9989852
Ndife <stilesndife@gmail.com>
2018-06-28 19:37:20 +0100

    added exports to the comment repo

--- repositories/CommentRepo.js
+++ repositories/CommentRepo.js
@@ -1,2 +1,10 @@
 const baseRepo = require('../repositories/BaseRepository');
 const model = require('../Models/Comment');
+
+function commentRepository(){
+
+}
+
+commentRepository.prototype = baseRepo(model);
+
+module.exports = new commentRepository();
