014dde92deb84c7c679005a4d67b78d29b7a9fe5
Ndife <stilesndife@gmail.com>
2018-06-28 08:42:37 +0100

    added method for adding users in userControllers

--- Controllers/UserController.js
+++ Controllers/UserController.js
@@ -1,21 +1,15 @@
-var model = require('../Models/User');
+const userService = require('../services/UserService');
 
-
-exports.addUser = (req, res)=>{
-    var data ={
+exports.addUsers = (req,res)=>{
+    data ={
         name: req.body.name,
         email: req.body.email,
-        password: req.body.password
-    }
-    model.create(data,(err)=>{
-        if(err) res.json({err:err, message:'user could not be created'});
-        res.json({message:'User created successfully'});
-    }); 
+        password: req.body.password,
+    };
+   return userService.addUser(data,res);
 }
 
-exports.getUsers = (req, res)=>{
-    model.find((err,users)=>{
-        if(err) res.json({err:err, message:"Something went wrong"});
-        res.json(users);
-    })
+exports.getUsers = (req,res)=>{
+    return userService.getAllUsers(req,res);
 }
+
