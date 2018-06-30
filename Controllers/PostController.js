068db93821bc0021332de847e8d7419db1098f58
Ndife <stilesndife@gmail.com>
2018-06-30 13:53:59 +0100

    added a post update method to postcontroller

--- Controllers/PostController.js
+++ Controllers/PostController.js
@@ -18,7 +18,16 @@ exports.getById = (req,res,id)=>{
     return postService.getPostById(req,res,id);
 }
 
-exports.getByparams = (req,res,option)=>{
+exports.getByparams = (req,res)=>{
     option = req.query;
     return postService.getByParams(req,res,option);
+}
+
+exports.updatePost = (req,res) =>{
+    id = {_id:req.params.id} 
+data = {
+    time: new Date(),
+    postBody: req.body.postBody,
+}
+return postService.updatePost(req,res,id,data);
 }
