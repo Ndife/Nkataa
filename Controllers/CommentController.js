d82fcee770f1ece07cd7f2d625e861d793ebffe7
Ndife <stilesndife@gmail.com>
2018-06-30 17:54:30 +0100

    added a method or controller to added comments

--- Controllers/CommentController.js
+++ Controllers/CommentController.js
@@ -1,2 +1,12 @@
 const commentService = require('../services/CommentService');
 
+module.exports.addComment = (req,res)=>{
+    comment = {
+        name: req.body.name,
+        date: new Date,
+        commentBody: req.body.commentBody,
+        userId: req.body.userId,
+        postId: req.body.userId,
+    }
+   return commentService.addComment(req,res,comment);
+}
