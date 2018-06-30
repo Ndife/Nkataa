95c62cc145d9d212f853b2aedbddcc91333307e6
Ndife <stilesndife@gmail.com>
2018-06-30 14:32:13 +0100

    added all route for posts to the post route file

--- routes/posts.js
+++ routes/posts.js
@@ -2,3 +2,10 @@ const express = require('express');
 const router = express.Router();
 var postController = require('../Controllers/PostController');
 
+/* GET posts listing */
+router.post('/addPost', postController.addPost);
+router.get('/getAllPost', postController.getPosts);
+router.get('/getPostById',postController.getById);
+router.get('/getPostByParams',postController.getByparams); 
+router.post('/updatePost/:id',postController.updatePost); 
+router.get('/deletePost',postController.deletePost);
