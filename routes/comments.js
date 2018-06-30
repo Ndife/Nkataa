e5a462767a7ad93ba349abbb1a370dd488dcbe52
Ndife <stilesndife@gmail.com>
2018-06-30 18:29:54 +0100

    added a route for the getcomments to comments.js

--- routes/comments.js
+++ routes/comments.js
@@ -3,5 +3,5 @@ const commentController = require('../Controllers/CommentController');
 var router = express.Router();
 
 router.post('/addComment',commentController.addComment);
-
+router.get('/getComments',commentController.getComments);
 module.exports = router;
