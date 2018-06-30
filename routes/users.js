d5295331e965e7573afe5985aea68bd2f35793ee
Ndife <stilesndife@gmail.com>
2018-06-30 08:50:57 +0100

    added router for getting a post

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var userController = require('../Controllers/UserController');
+var postController = require('../Controllers/PostController');
 
 /* GET users listing. */
 router.post('/create',userController.addUsers); 
@@ -9,5 +10,10 @@ router.get('/getById',userController.getById);
 router.get('/getUsersByParams',userController.getUserByParams);
 router.get('/delete',userController.deleteById);
 router.post('/update/:id',userController.updateUser);
+
+/* GET posts listing */
+router.post('/addPost', postController.addPost);
+router.get('/getAllPost', postController.getPosts);
+
 
 module.exports = router;
