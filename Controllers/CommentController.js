4d9e7b52784b809fcf104526f6ae5a68e2f1cf34
Ndife <stilesndife@gmail.com>
2018-06-30 18:47:49 +0100

    added a method for getcomments using Id to comment controller

--- Controllers/CommentController.js
+++ Controllers/CommentController.js
@@ -1,6 +1,6 @@
 const commentService = require('../services/CommentService');
 
-module.exports.addComment = (req,res)=>{
+exports.addComment = (req,res)=>{
     comment = {
         name: req.body.name,
         date: new Date,
@@ -11,6 +11,11 @@ module.exports.addComment = (req,res)=>{
    return commentService.addComment(req,res,comment);
 }
 
-module.exports.getComments = (req,res)=>{
+exports.getComments = (req,res)=>{
 return commentService.getComments(req,res);
+}
+
+exports.getById = (req,res)=>{
+    id = {_id:req.query._id};
+return commentService.getCommentById(req,res,id);
 }
