e73745baeb5635c1282933d7004b1a488214bd2a
Ndife <stilesndife@gmail.com>
2018-06-30 18:46:18 +0100

    added method for getting comments using Id

--- services/CommentService.js
+++ services/CommentService.js
@@ -1,15 +1,22 @@
 const commentRepo = require('../repositories/CommentRepo');
 
-module.exports.addComment = (req,res,comment)=>{
+exports.addComment = (req,res,comment)=>{
     commentRepo.add(comment,(err)=>{
         if(err) res.json({err:err, message:'failed to add comment'});
         res.json({message:'comment added successfully'});
     });
 }
 
-module.exports.getComments = (req,res)=>{
+exports.getComments = (req,res)=>{
     commentRepo.get({},(err,comments)=>{
         if(err) res.json({err:err, message:'Sorry, something went wrong'});
         res.json(comments);
     });
+}
+
+exports.getCommentById= (req,res,id)=>{
+commentRepo.getById(id,(err,comment)=>{
+    if(err) res.json({err:err, message:'Faild to get Comment by Id'});
+    res.json(comment);
+});
 }
