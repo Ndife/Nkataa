f7551de3342132b532e9f96f5c4b98957f8807ee
Ndife <stilesndife@gmail.com>
2018-06-30 17:57:10 +0100

    changed posts to postId

--- Models/User.js
+++ Models/User.js
@@ -4,7 +4,7 @@ var UserSchema = Mongoose.Schema({
     name: String,
     email: {type: String, unique:true},
     password: String,
-    posts: [{type:Mongoose.Schema.Types.ObjectId, ref:'Post'}]
+    postsId: [{type:Mongoose.Schema.Types.ObjectId, ref:'Post'}]
 });
 
 module.exports = Mongoose.model('User',UserSchema);
